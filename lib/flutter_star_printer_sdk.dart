8e529b6319fe060153460c56061fa17dad7d3679
Sankaranarayanan-P <sankaranarayananp2021@gmail.com>
2023-05-30 11:54:41 +0530

    methods added

--- lib/flutter_star_printer_sdk.dart
+++ lib/flutter_star_printer_sdk.dart
@@ -6,9 +6,50 @@
 // https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
 
 import 'flutter_star_printer_sdk_platform_interface.dart';
+import 'package:flutter_star_printer_sdk/models/flutter_star_printer.dart';
 
 class FlutterStarPrinterSdk {
+  /// This function returns the platform version of the FlutterStarPrinterSdkPlatform instance as a
+  /// Future<String?>.
+  ///
+  /// Returns:
+  ///   A `Future` object that will eventually contain a `String` or `null` value.
   Future<String?> getPlatformVersion() {
     return FlutterStarPrinterSdkPlatform.instance.getPlatformVersion();
+  }
+
+  /// This function returns a Future object that discovers a printer using the
+  /// FlutterStarPrinterSdkPlatform instance.
+  ///
+  /// Returns:
+  ///   A `Future` object that will eventually resolve to a `FlutterStarPrinter` object after
+  /// discovering a printer using the `FlutterStarPrinterSdkPlatform` instance.
+  Future<FlutterStarPrinter> discoverPrinter() async {
+    return FlutterStarPrinterSdkPlatform.instance.discoverPrinter();
+  }
+
+  /// This function attempts to connect to a printer using the FlutterStarPrinterSdkPlatform and returns
+  /// a boolean indicating whether the connection was successful or not.
+  ///
+  /// Returns:
+  ///   A `Future<bool>` object is being returned.
+  Future<bool> connectPrinter() async {
+    return FlutterStarPrinterSdkPlatform.instance.connectPrinter();
+  }
+
+  /// This function disconnects the printer using the FlutterStarPrinterSdkPlatform instance.
+  ///
+  /// Returns:
+  ///   A `Future<bool>` object is being returned.
+  Future<bool> disconnectPrinter() async {
+    return FlutterStarPrinterSdkPlatform.instance.disconnectPrinter();
+  }
+
+  /// This function calls the print method of the FlutterStarPrinterSdkPlatform instance.
+  ///
+  /// Returns:
+  ///   A `Future<void>` object is being returned.
+  Future<void> print() async {
+    return FlutterStarPrinterSdkPlatform.instance.print();
   }
 }
--- lib/flutter_star_printer_sdk_method_channel.dart
+++ lib/flutter_star_printer_sdk_method_channel.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_star_printer_sdk/models/flutter_star_printer.dart';
 
 import 'flutter_star_printer_sdk_platform_interface.dart';
 
 /// An implementation of [FlutterStarPrinterSdkPlatform] that uses method channels.
-class MethodChannelFlutterStarPrinterSdk extends FlutterStarPrinterSdkPlatform {
-
+class MethodChannelFlutterStarPrinterSdk
+    implements FlutterStarPrinterSdkPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
-  final methodChannel = const MethodChannel('co.uk.ferns.flutter_plugins/flutter_star_printer_sdk');
+  final methodChannel = const MethodChannel(
+      'co.uk.ferns.flutter_plugins/flutter_star_printer_sdk');
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
+  }
+
+  @override
+  Future<bool> connectPrinter() {
+    // TODO: implement connectPrinter
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> disconnectPrinter() {
+    // TODO: implement disconnectPrinter
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<FlutterStarPrinter> discoverPrinter() {
+    // TODO: implement discoverPrinter
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> print() {
+    // TODO: implement print
+    throw UnimplementedError();
   }
 }
